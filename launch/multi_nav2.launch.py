3a5694942cd34e25512f2025f02991f23db16cf6
Elitsa Venchovs <elica_vladi@abv.bg>
2025-11-15 17:54:55 +0200

    try to fix missing map in rviz (fail)

--- launch/multi_nav2.launch.py
+++ launch/multi_nav2.launch.py
@@ -1,12 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     map_file = '/home/evenchova/turtlebot3_ws/src/patrol_sim/maps/map.yaml'
 
     # --- Shared global map server ---
@@ -15,27 +12,36 @@ def generate_launch_description():
         executable='map_server',
         name='map_server',
         output='screen',
-        parameters=[{'yaml_filename': map_file, 'use_sim_time': True}]
+        parameters=[{
+            'yaml_filename': map_file,
+            'topic_name': 'map',
+            'frame_id': 'map',
+            'use_sim_time': True
+        }]
+    )
+
+    # Nav2 lifecycle manager
+    amcl_nodes = [f'TB3_{i+1}/amcl' for i in range(4)]
+    nav2_lifecycle_manager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
+        parameters=[{
+            'autostart': True,
+            'use_sim_time': True,
+            'bond_timeout': 0.0,
+            'node_names': ['map_server'] + amcl_nodes
+        }]
     )
 
     robots = [f'TB3_{i+1}' for i in range(4)]
     nodes = []
 
     for ns in robots:
-        # Nav2 bringup per robot
-        bringup = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
-            ),
-            launch_arguments={
-                'namespace': ns,
-                'use_namespace': 'True',
-                'map': map_file,
-                'use_sim_time': 'True',
-            }.items()
-        )
+        nav2_yaml = f'/home/evenchova/turtlebot3_ws/src/patrol_sim/config/{ns}_amcl_config.yaml'
 
-        # Initial pose publisher per robot
+        # Initial pose publisher
         init_pose = Node(
             package='patrol_sim',
             executable='set_initial_pose',
@@ -45,6 +51,20 @@ def generate_launch_description():
             parameters=[{'robot_ns': ns}]
         )
 
-        nodes += [bringup, init_pose]
+        # Namespaced AMCL with map remapping
+        nav2_amcl = Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            namespace=ns,
+            output='screen',
+            parameters=[nav2_yaml],
+            remappings=[
+                ('/map', 'map'),
+                ('/map_updates', 'map_updates')
+            ]
+        )
 
-    return LaunchDescription([map_server] + nodes)
+        nodes += [init_pose, nav2_amcl]
+
+    return LaunchDescription([map_server] + nodes + [nav2_lifecycle_manager])
